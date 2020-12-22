a961baa7cd3ed4094a6bda2e2a8be87faf11446f
leonteAndrei <45095003+leonteAndrei@users.noreply.github.com>
2020-12-22 17:28:01 +0200

    Most of the parameters must be moved in a new line.

--- src/CSharp.ApiRequest.Library/IHttpClientRequest.cs
+++ src/CSharp.ApiRequest.Library/IHttpClientRequest.cs
@@ -8,26 +8,39 @@ namespace CSharp.ApiRequest.Library
     {
         Task GetAsync(IRequestInfo requestInfo, CancellationToken cancellationToken = default);
         Task<TResult> GetAsync<TResult>(IRequestInfo requestInfo, CancellationToken cancellationToken = default);
-        Task<TResult> GetAsync<TRequest, TResult>(IRequestInfo requestInfo, TRequest body, CancellationToken cancellationToken = default);
+        Task<TResult> GetAsync<TRequest, TResult>(
+            IRequestInfo requestInfo, TRequest body, CancellationToken cancellationToken = default);
 
         Task PostAsync(IRequestInfo requestInfo, CancellationToken cancellationToken = default);
-        Task PostAsync<TRequest>(IRequestInfo requestInfo, TRequest body, CancellationToken cancellationToken = default);
-        Task<TResult> PostAsync<TResult>(IRequestInfo requestInfo, CancellationToken cancellationToken = default);
-        Task<TResult> PostAsync<TRequest, TResult>(IRequestInfo requestInfo, TRequest body, CancellationToken cancellationToken = default);
+        Task PostAsync<TRequest>(
+            IRequestInfo requestInfo, TRequest body, CancellationToken cancellationToken = default);
+        Task<TResult> PostAsync<TResult>(
+            IRequestInfo requestInfo, CancellationToken cancellationToken = default);
+        Task<TResult> PostAsync<TRequest, TResult>(
+            IRequestInfo requestInfo, TRequest body, CancellationToken cancellationToken = default);
 
         Task PutAsync(IRequestInfo requestInfo, CancellationToken cancellationToken = default);
-        Task PutAsync<TRequest>(IRequestInfo requestInfo, TRequest body, CancellationToken cancellationToken = default);
-        Task<TResult> PutAsync<TResult>(IRequestInfo requestInfo, CancellationToken cancellationToken = default);
-        Task<TResult> PutAsync<TRequest, TResult>(IRequestInfo requestInfo, TRequest body, CancellationToken cancellationToken = default);
+        Task PutAsync<TRequest>(
+            IRequestInfo requestInfo, TRequest body, CancellationToken cancellationToken = default);
+        Task<TResult> PutAsync<TResult>(
+            IRequestInfo requestInfo, CancellationToken cancellationToken = default);
+        Task<TResult> PutAsync<TRequest, TResult>(
+            IRequestInfo requestInfo, TRequest body, CancellationToken cancellationToken = default);
 
         Task PatchAsync(IRequestInfo requestInfo, CancellationToken cancellationToken = default);
-        Task PatchAsync<TRequest>(IRequestInfo requestInfo, TRequest body, CancellationToken cancellationToken = default);
-        Task<TResult> PatchAsync<TResult>(IRequestInfo requestInfo, CancellationToken cancellationToken = default);
-        Task<TResult> PatchAsync<TRequest, TResult>(IRequestInfo requestInfo, TRequest body, CancellationToken cancellationToken = default);
+        Task PatchAsync<TRequest>(
+            IRequestInfo requestInfo, TRequest body, CancellationToken cancellationToken = default);
+        Task<TResult> PatchAsync<TResult>(
+            IRequestInfo requestInfo, CancellationToken cancellationToken = default);
+        Task<TResult> PatchAsync<TRequest, TResult>
+            (IRequestInfo requestInfo, TRequest body, CancellationToken cancellationToken = default);
 
         Task DeleteAsync(IRequestInfo requestInfo, CancellationToken cancellationToken = default);
-        Task DeleteAsync<TRequest>(IRequestInfo requestInfo, TRequest body, CancellationToken cancellationToken = default);
-        Task<TResult> DeleteAsync<TResult>(IRequestInfo requestInfo, CancellationToken cancellationToken = default);
-        Task<TResult> DeleteAsync<TRequest, TResult>(IRequestInfo requestInfo, TRequest body, CancellationToken cancellationToken = default);
+        Task DeleteAsync<TRequest>(
+            IRequestInfo requestInfo, TRequest body, CancellationToken cancellationToken = default);
+        Task<TResult> DeleteAsync<TResult>(
+            IRequestInfo requestInfo, CancellationToken cancellationToken = default);
+        Task<TResult> DeleteAsync<TRequest, TResult>(
+            IRequestInfo requestInfo, TRequest body, CancellationToken cancellationToken = default);
     }
 }
